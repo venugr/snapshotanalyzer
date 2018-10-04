bcde738a6a1e037e2d84c0848ce6a944f27ee415
Lella, Venugopala <Venugopala.Lella@ellucian.com>
2018-10-04 08:01:12 +0530

    Filter instances by project tag. Also stop, start and list instances.

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -3,18 +3,75 @@ import click
 
 session = boto3.Session ( profile_name='default' )
 ec2 = session.resource ( 'ec2' )
-@click.command()
-def list_instances():
-    "List EC2 instances"
-    for i in ec2.instances.all():
-       print (",".join ((
-          i.id,
-          i.instance_type,
-          i.placement['AvailabilityZone'],
-          i.state['Name'],
-          i.public_dns_name )))
-    return
+
+def listInstances(project):
+   instances = []
+
+   if project:
+      filters = [ {'Name': 'tag:Project', 'Values': [project] } ]
+      instances = ec2.instances.filter( Filters = filters )
+   else:
+      instances = ec2.instances.all()
+    
+   return instances
+
+
+@click.group()
+def instances():
+   """Commands for instances"""
+
+@instances.command('list')
+@click.option ( '--project', default=None,
+                help="Only Instacnes for project ( tag Project:<name> )" ) 
+
+def list_instances( project ):
+   "List EC2 instances"
+
+   instances = listInstances( project ) 
+
+   for i in instances:
+      tags = { t['Key']: t['Value'] for t in i.tags or [] } 
+      print (",".join ((
+         i.id,
+         i.instance_type,
+         i.placement['AvailabilityZone'],
+         i.state['Name'],
+         i.public_dns_name,
+         tags.get('Project', '<no project>')  )))
+   return
+
+
+@instances.command('stop')
+@click.option ( '--project', default=None,
+                help="Only Instacnes for project ( tag Project:<name> )" )
+
+def stop_instances( project ):
+   "Stop EC2 Instances"
+
+   instances = listInstances( project ) 
+
+   for i in instances:
+      print ( "Stopping {0}..." . format ( i.id ) ) 
+      i.stop()
+
+   return
+
+
+@instances.command('start')
+@click.option ( '--project', default=None,
+                help="Only Instacnes for project ( tag Project:<name> )" )
+
+def stop_instances( project ):
+   "Start EC2 Instances"
+
+   instances = listInstances( project )
+
+   for i in instances:
+      print ( "Starting {0}..." . format ( i.id ) )
+      i.start()
+
+   return
 
 
 if __name__ == "__main__":
-   list_instances()
+   instances()
